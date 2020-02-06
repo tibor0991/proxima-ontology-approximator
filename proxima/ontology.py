84834186187a7b003e8b7960843ff6143956959d
Gianfranco Damato <gianfranco.damato@hotmail.it>
2020-02-06 15:27:57 +0100

    Update ontology.py

--- proxima/ontology.py
+++ proxima/ontology.py
@@ -3,11 +3,10 @@ import pandas as pd
 import numpy as np
 import types
 
-import time
-
 
 def render_colon(e: or2.entity) -> str:
     return "%s:%s" % (e.namespace.name, e.name)
+
 
 class DataRemapper:
     def __init__(self, values_dict):
@@ -15,6 +14,7 @@ class DataRemapper:
 
     def __call__(self, value):
         return self.values_dict[value]
+
 
 _PT_UNCERTAIN = 'UNCERTAIN'
 _PT_TRUE = 'TRUE'
@@ -24,20 +24,47 @@ default_remapper = DataRemapper({_PT_TRUE: 1, _PT_FALSE: 0, _PT_UNCERTAIN: 0.5})
 
 
 class OntologyManager:
-    def __init__(self, path, remapper=default_remapper):
+    def __init__(self, path, remapper=default_remapper, **kwargs):
         # loads an ontology and stores classes and individuals in two dictionaries
         or2.set_render_func(render_colon)  # WARNING: unset me
-        onto = or2.get_ontology(path).load()
-        classes = {str(c): c for c in onto.classes()}
-        individuals = {str(i): i for i in onto.individuals()}
+        _onto = or2.get_ontology(path).load()
 
-        # build the projection table
-        projection_table = pd.DataFrame(data=_PT_UNCERTAIN, index=individuals.keys(),
-                                        columns=classes.keys())
-        # run the reasoner
-        with onto:
+        # consistency check
+        with _onto:
             or2.sync_reasoner_pellet()
 
+        # saves a pair of name/value for each class and individual
+        _classes = {str(c): c for c in _onto.classes()}
+        _individuals = {str(i): i for i in _onto.individuals()}
+
+        # builds the projection table
+        _projection_table = pd.DataFrame(data=_PT_UNCERTAIN, index=_individuals.keys(),
+                                        columns=_classes.keys())
+
+        # here goes the projection method
+
+
+        _projection_table = _projection_table.apply(np.vectorize(remapper))
+
+        """ # scheduled for deletion
+        
+        projection_table = projection_table.apply(np.vectorize(remapper))
+        """
+        print(list(onto.classes()))
+        or2.set_render_func(or2.default_render_func)  # unsets the render function
+
+        # saves the variables in the object
+        self.onto = onto
+        self.projection_table = projection_table
+        self.classes = classes
+        self.individuals = individuals
+        self.remapper = remapper
+
+    def _projection_by_outsourced_table(self, path, table):
+        table =
+        return
+
+    def _projection_by_disjunction(self, onto, classes, table):
         # build the disjunction lookup dict
         disjunctions = {str(c): set() for c in onto.classes()}
         for pair in onto.disjoint_classes():
@@ -53,35 +80,41 @@ class OntologyManager:
                 false_set = false_set.union(d.instances())
 
             for t in true_set:
-                projection_table.at[str(t), c_name] = _PT_TRUE
+                table.at[str(t), c_name] = _PT_TRUE
             for f in false_set:
-                projection_table.at[str(f), c_name] = _PT_FALSE
+                table.at[str(f), c_name] = _PT_FALSE
         # 6.9 seconds for .instances() method
-        projection_table = projection_table.apply(np.vectorize(remapper))
+        return
 
-        or2.set_render_func(or2.default_render_func)  # unsets the render function
+    def _projection_by_reasoning(self, onto, classes, table):
+        # list that holds all the references to the not_ classes
+        classes_with_complements = []
+        with onto:
+            for c_name, c_item in classes.items():
+                neg_class = types.new_class('NOT_' + c_item.name, (or2.Thing,))
+                neg_class.equivalent_to = [or2.Not(c_item)]
+                or2.AllDisjoint([c_item, neg_class])
+                classes_with_complements.append((c_name, c_item, neg_class))
 
-        # saves the variables in the object
-        self.onto = onto
-        self.projection_table = projection_table
-        self.classes = classes
-        self.individuals = individuals
-        self.remapper = remapper
+        # run the reasoner
+        with onto:
+            or2.sync_reasoner_pellet()
+
+        for c_name, c, n in classes_with_complements:
+            true_set = set(c.instances())
+            false_set = set(n.instances())
+
+        for t in true_set:
+            table.at[str(t), c_name] = _PT_TRUE
+        for f in false_set:
+            table.at[str(f), c_name] = _PT_FALSE
+
 
     def insert_approximated_concept(self, concept_name, upper_elements, lower_elements):
-        """
-        Inserts a pair of classes which represent an approximate concept in the form of C = < U, L >, where:
-        - C is the approximate concept
-        - U is the Upper approximation of C (named as "Possibly"+[base concept name] )
-        - L is the Uower approximation of C (named as "Definitively"+[base_concept_name] )
-        :param concept_name: Base name of the approximate concept
-        :param upper_elements: sets of individuals that belong to the upper approximation
-        :param lower_elements: sets of individuals that belong to the lower approximation
-        :return: The modified ontology O'
-        """
+
         # build the two approximation classes
-        upper_name = "Possibly" + concept_name
-        lower_name = "Definitively" + concept_name
+        upper_name = "Possibly_" + concept_name
+        lower_name = "Definitively_" + concept_name
         # add the classes to the ontology
         UpperClass = types.new_class(upper_name, (or2.Thing,))
         LowerClass = types.new_class(lower_name, (UpperClass,))
